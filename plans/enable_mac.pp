567105cbe0445abcc74b52d2105bba8beffd599c
Marc McKinley <marcmckinley@me.com>
2020-07-05 11:12:25 +0100

    update

--- plans/enable_mac.pp
+++ plans/enable_mac.pp
@@ -1,13 +1,13 @@
 plan tools::enable_mac(
   TargetSpec $targets
 ) {
-  run_task('tools::prep', $targets)
-  run_task('tools::tweaks', $targets, '_run_as' => 'root')
-  run_task('tools::icons', $targets)
-  run_task('tools::theme', $targets)
-  run_task('tools::dash_to_dock', $targets)
-  run_task('tools::button_layout', $targets)
-  run_task('tools::keyboard_shortcuts', $targets)
-  run_task('tools::animations', $targets)
-  run_task('tools::workspaces', $targets)
+  return run_task('tools::prep', $targets)
+  return run_task('tools::tweaks', $targets, '_run_as' => 'root', '_catch_errors' => true)
+  return run_task('tools::icons', $targets)
+  return run_task('tools::theme', $targets)
+  return run_task('tools::dash_to_dock', $targets)
+  return run_task('tools::button_layout', $targets)
+  return run_task('tools::keyboard_shortcuts', $targets)
+  return run_task('tools::animations', $targets)
+  return run_task('tools::workspaces', $targets)
   }
