b7865db2a5672d06b60882d975604c73802560ff
Marc McKinley <marc@puppet.com>
2020-07-06 16:13:04 +0100

    Update keyboard_shortcuts.sh

--- tasks/keyboard_shortcuts.sh
+++ tasks/keyboard_shortcuts.sh
@@ -5,7 +5,8 @@ keyb='*******Attempting to re-configure keyboard layout*******'
 
 #to find current setting - gsettings list-recursively | egrep "Primary.*n"
 echo -e "\n$keyb\n"
-gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:swap_lalt_lctl']"
+org.gnome.desktop.input-sources xkb-options ['ctrl:swap_lwin_lctl', 'ctrl:swap_rwin_rctl']
+#gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:swap_lalt_lctl']"
 gsettings set org.gnome.desktop.wm.keybindings minimize "['<Primary>h']"
 gsettings set org.gnome.desktop.wm.keybindings close "['<Primary>q']"
 gsettings set org.gnome.Terminal.Legacy.Keybindings:/org/gnome/terminal/legacy/keybindings/ copy '<Primary>c'
